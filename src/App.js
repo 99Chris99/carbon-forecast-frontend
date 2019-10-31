f58bb090dbd743c2ab8a359c34133501545f53f4
99chris99 <chrishodge87@gmail.com>
2019-10-31 09:42:52 +0000

    components and basic structure built

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Forecast from './components/Forecast';
+import About from './components/About';
+import Advice from './components/Advice';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+             <h1>Hi there, I'm the home page, hope you are well!</h1>   
+            </div>
+        );
+    }
+}
+
+export default Home;
